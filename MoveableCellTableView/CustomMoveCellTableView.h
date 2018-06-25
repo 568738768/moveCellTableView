8630aa6f8c45942ec3292cf5f822d9d38d7c91ff
lishishi <568738768@qq.com>
2018-06-25 15:07:38 +0800

    提交

---
+++ MoveableCellTableView/CustomMoveCellTableView.h
@@ -0,0 +1,13 @@
+//
+//  CustomMoveCellTableView.h
+//  LSMovableCellTableView
+//
+//  Created by Liss on 16/2/16.
+//  Copyright © 2016年 Liss. All rights reserved.
+//
+
+#import "LSMovableCellTableView.h"
+
+@interface CustomMoveCellTableView : LSMovableCellTableView
+
+@end
